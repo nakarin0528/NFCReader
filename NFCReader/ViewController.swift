528212e85e5facb8b47b4779d3c134b210af04db
nakarin0528 <n1510121@edu.cc.uec.ac.jp>
2017-12-14 02:10:21 +0900

    first commit

---
+++ NFCReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFCReader
+//
+//  Created by Iko Nakari on 2017/12/14.
+//  Copyright © 2017年 Iko Nakari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
